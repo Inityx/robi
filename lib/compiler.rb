c34f77d418794b5f9b2666f9c24a0d7ff52e8408
Andrew Tolvstad <CTGBandy@gmail.com>
2016-11-26 06:14:47 -0800

    It works, but with kindlegen warnings and no CSS

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -3,13 +3,14 @@ require 'fileutils'
 
 module Rindle
   class Compiler
-    HTML = 'index.html'
-    METADATA = 'metadata.opf'
-    STYLE = 'stylesheet.css'
+    def initialize(title_slug, stylesheet_source)
+      @dest_dir = "./#{title_slug}"
 
-    def initialize(dest_dir, stylesheet)
-      @dest_dir = dest_dir
-      @stylesheet = stylesheet
+      @html_file = 'index.html'
+      @stylesheet_source = stylesheet_source
+      @stylesheet_file = File.basename(@stylesheet_source)
+      @table_of_contents_file = 'tableofcontents.ncx'
+      @metadata_file = 'metadata.opf'
     end
 
     def compile(title, posts)
@@ -19,10 +20,13 @@ module Rindle
       puts '  Building metadata...'
       metadata = build_metadata(title)
 
-      puts '  Assembling files...'
-      assemble(metadata, html)
+      puts '  Building Table of Contents...'
+      table_of_contents = build_table_of_contents(title, posts)
 
-      "#{@dest_dir}/#{METADATA}"
+      puts '  Assembling files...'
+      assemble(metadata, html, table_of_contents)
+
+      "#{@dest_dir}/#{@metadata_file}"
     end
 
     def build_html(title, posts)
@@ -32,7 +36,7 @@ module Rindle
             doc.title { doc.text title }
             doc.link(
               rel: 'stylesheet',
-              href: STYLE,
+              href: @stylesheet_file,
               type: 'text/css'
             )
           }
@@ -49,7 +53,7 @@ module Rindle
               doc.ol {
                 posts.each do |post|
                   doc.li {
-                    doc.a(href: "\##{post.uid}") {
+                    doc.a(href: "##{post.uid}") {
                       doc.text post.title
                     }
                   }
@@ -61,7 +65,7 @@ module Rindle
                 doc.div(class: 'post', id: post.uid) {
                   doc.h1 { doc.text post.title }
                   doc.h2 { doc.text "by #{post.author}" }
-                  doc.p {
+                  doc.div {
                     body = Nokogiri::HTML::DocumentFragment.parse(post.body)
                     doc << body.content
                   }
@@ -76,34 +80,92 @@ module Rindle
     def build_metadata(title)
       Nokogiri::XML::Builder.new { |xml|
         xml.root {
-          xml.product {
-            # TODO: fix metadata with namespaces
+          xml.package(
+            :'unique-identifier' => title,
+            :'xmlns:opf' => 'http://www.idpf.org/2007/opf',
+            :'xmlns:asd' => 'http://www.idpf.org/asdfaf'
+          ) {
+            xml.metadata {
+              xml.send(
+                :'dc-metadata',
+                :'xmlns:dc' => 'http://purl.org/metadata/dublin_core',
+                :'xmlns:oebpackage' => 'http://openebook.org/namespaces/oeb-package/1.0/'
+              ) {
+                xml.send(:'dc:Title', title)
+                xml.send(:'dc:Language', 'en')
+                xml.send(:'dc:Creator', 'Reddit')
+                xml.send(:'x-metadata')
+              }
+            }
             xml.manifest {
+              # xml.item(
+              #   :id => 'content',
+              #   :'media-type' => 'text/x-oeb1-document',
+              #   :href => "#{HTML}#tableofcontents"
+              # )
+              xml.item(
+                :id => 'ncx',
+                :'media-type' => 'application/x-dtbncx+xml',
+                :href => @table_of_contents_file
+              )
               xml.item(
                 :id => 'text',
                 :'media-type' => 'text/x-oeb1-document',
-                :href => "#{HTML}#postwrapper"
+                :href => "#{@html_file}"
               )
             }
-            xml.spine  { # TODO: ToC
+            xml.spine(toc: 'ncx')  {
+              # xml.itemref(idref: 'content')
               xml.itemref(idref: 'text')
             }
             xml.guide {
-              xml.reference(type: 'text', title: title, href: HTML)
+              xml.reference(type: 'toc', title: 'Table of Contents', href: @table_of_contents_file)
+              xml.reference(type: 'text', title: title, href: @html_file)
             }
           }
         }
       }.to_xml
     end
 
-    def assemble(metadata, html)
-      Dir.mkdir(@dest_dir) unless Dir.exist?(@dest_dir)
-      File.open("#{@dest_dir}/#{HTML}", 'w') { |file| file.write(html) }
-      File.open("#{@dest_dir}/#{METADATA}",  'w') { |file| file.write(metadata) }
-      FileUtils.cp(@stylesheet, "#{@dest_dir}/#{STYLE}")
+    def build_table_of_contents(title, posts)
+      Nokogiri::XML::Builder.new { |xml|
+        xml.doc.create_internal_subset(
+          'ncx',
+          '-//NISO//DTD ncx 2005-1//EN',
+          'http://www.daisy.org/z3986/2005/ncx-2005-1.dtd'
+        )
+        xml.ncx(xmlns: 'http://www.daisy.org/z3986/2005/ncx/', version: '2005-1') {
+          xml.docTitle {
+            xml.text_ title
+          }
+          xml.navMap {
+            # xml.navPoint(id: 'toc', playOrder: 1) {
+            #   xml.navLabel {
+            #     xml.text_ 'Table of Contents'
+            #   }
+            #   xml.content(src: @html_file)
+            # }
+            posts.zip(1..Float::INFINITY).each do |post, index|
+              xml.navPoint(id: post.uid, playOrder: index) {
+                xml.navLabel {
+                  xml.text_ post.title
+                }
+                xml.content(src: "#{@html_file}##{post.uid}")
+              }
+            end
+          }
+        }
+      }.to_xml
     end
 
-    def convert
+    def assemble(metadata, html, table_of_contents)
+      Dir.mkdir(@dest_dir) unless Dir.exist?(@dest_dir)
+
+      File.open("#{@dest_dir}/#{@html_file}", 'w') { |file| file.write(html) }
+      File.open("#{@dest_dir}/#{@metadata_file}",  'w') { |file| file.write(metadata) }
+      File.open("#{@dest_dir}/#{@table_of_contents_file}",  'w') { |file| file.write(table_of_contents) }
+
+      FileUtils.cp(@stylesheet_source, "#{@dest_dir}/#{@stylesheet_file}")
     end
 
     def simple_plural(word, count)
